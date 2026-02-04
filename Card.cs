0f4e558baec132bcc5a289ede45499611ef1c0d2
Kletca <h3yseilend@gmail.com>
2026-02-04 13:50:55 +0200

    start

---
+++ Card.cs
@@ -0,0 +1,30 @@
+﻿namespace BankCredit;
+
+public class Card
+{
+    private int _creditLimit;
+    private bool _usedCredit;
+
+    public Card(int creditLimit, bool usedCredit)
+    {
+        _creditLimit = creditLimit;
+        _usedCredit = usedCredit;
+    }
+    
+    public bool AvailableCredit
+    {
+        get { return _usedCredit; }
+    }
+
+    public void TakeCredit(int amount)
+    {
+        _usedCredit = true;
+        _creditLimit -= amount;
+    }
+
+    public void PayCredit(int amount)
+    {
+        _usedCredit = false;
+        _creditLimit += amount;
+    }
+}
---
+++ CreditCard.cs
@@ -0,0 +1,9 @@
+﻿namespace BankCredit;
+
+public class CreditCard:Card
+{
+    public CreditCard(int creditLimit, bool usedCredit) : base(creditLimit, usedCredit)
+    {
+        
+    }
+}
